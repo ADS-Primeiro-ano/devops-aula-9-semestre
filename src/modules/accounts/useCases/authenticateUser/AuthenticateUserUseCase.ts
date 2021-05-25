86c7f8b60687dee653069a0c6f578cd7461e3f4d
Juliano Ramos <490meister@gmail.com>
2021-05-25 13:16:52 -0300

    feat(Chapter-III): added JWT

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;    
+  },
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    
+    if (!user) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const passwordMatch = password === user.password;
+
+    if (!passwordMatch) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const token = sign({}, "7db275fd895f2600a1c400e32165530d", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      }
+    };
+
+    return tokenReturn;
+  }
+};
+
+export { AuthenticateUserUseCase };
