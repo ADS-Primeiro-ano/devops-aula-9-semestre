981482acdba11b6d23c08c5ec894c0f841eb3487
Juliano Ramos <490meister@gmail.com>
2021-06-01 09:21:46 -0300

    feat(Chapter-V): added coverage

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { EtherealMailProvider } from "@shared/container/providers/MailProvider/implementations/EtherealMailProvider";
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory";
+import { AppError } from "@shared/errors/AppError";
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase"
+
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let dateProvider: DayjsDateProvider;
+let mailProvider: MailProviderInMemory;
+
+describe("Send Forgot Mail", () => {
+
+  beforeEach(() => {
+    usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    dateProvider = new DayjsDateProvider();
+    mailProvider = new MailProviderInMemory();
+    sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
+      usersRepositoryInMemory,
+      usersTokensRepositoryInMemory,
+      dateProvider,
+      mailProvider
+    );
+  });
+
+  it("should be able to send a forgot password mail to user", async () => {
+
+    const sendMail = spyOn(mailProvider, "sendMail");
+
+    await usersRepositoryInMemory.create({
+      driver_license: "456465",
+      email: "asdasd@asd.pr",
+      name: "asdasdasd",
+      password: "1234",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("asdasd@asd.pr");
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+
+  it("should not be able to send an email if user does not exists", async () => {
+    await expect(sendForgotPasswordMailUseCase.execute("asasdasd@asd.pr"))
+      .rejects.toEqual(new AppError("User does not exists!"));
+  });
+
+  it("should be able to create an users token", async () => {
+    const generateTokenMail = spyOn(usersTokensRepositoryInMemory, "create");
+
+    await usersRepositoryInMemory.create({
+      driver_license: "234234",
+      email: "fff@asd.pr",
+      name: "rrrr",
+      password: "1234",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("fff@asd.pr");
+
+    expect(generateTokenMail).toBeCalled();
+  });
+});
--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -37,7 +37,7 @@ describe("List Categories", () => {
         password: "admin",
       });
 
-    const { token } = responseToken.body;
+    const { refresh_token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
@@ -46,7 +46,7 @@ describe("List Categories", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${refresh_token}`,
       });
 
     expect(response.status).toBe(201);
@@ -59,7 +59,7 @@ describe("List Categories", () => {
         password: "admin",
       });
 
-    const { token } = responseToken.body;
+    const { refresh_token } = responseToken.body;
 
     await request(app)
       .post("/categories")
@@ -68,7 +68,7 @@ describe("List Categories", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${refresh_token}`,
       });
 
     const response = await request(app)
