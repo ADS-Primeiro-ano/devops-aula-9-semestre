95e7e720e60d099d6e81bd6814521cd681f4ee73
Juliano Ramos <490meister@gmail.com>
2021-06-02 17:13:24 -0300

    feat(Chapter-VI): added rate limiter and redis

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,14 +9,19 @@ import createConnection from "@shared/infra/typeorm";
 
 import "@shared/container";
 
-import upload from "@config/upload";
-import { router } from '@shared/infra/http/routes';
-import swaggerFile from '../../../swagger.json';
 import { AppError } from '@shared/errors/AppError';
+import upload from "@config/upload";
+
+import swaggerFile from '../../../swagger.json';
+import { router } from '@shared/infra/http/routes';
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 
 createConnection();
 
 const app = express();
+
+app.use(rateLimiter);
+app.use(cors());
 
 app.use(express.json());
 
@@ -25,7 +30,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
-app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
